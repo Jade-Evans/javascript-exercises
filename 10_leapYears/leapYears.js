fd05861251227fd1fa2f4f23bfb6b53065789da8
Jade-Evans <jpgabb@msn.com>
2025-05-28 15:45:16 +0100

    feat(leapYears): Use if..else conditional statements to work out if argument passed is a leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear;
+    if (year%4 !== 0){
+        isLeapYear = false;
+    }
+    else if (year%100 ===0 && year%400===0){
+        isLeapYear = true;
+    }
+    else if (year%100===0){
+        isLeapYear = false;
+    }
+    else  isLeapYear = true;
+    return isLeapYear;
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
