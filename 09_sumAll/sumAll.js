1376c769fd9ea0ec2d2f766384eb4d2eb0fb365e
Jade-Evans <jpgabb@msn.com>
2025-05-28 12:26:02 +0100

    feat(sumAll): add function to sum all numbers between two positive integers

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (a <0 || b<0 || a%1 !== 0 || b%1 !==0 || typeof a !=="number" || typeof b !=="number"){
+    
+        return "ERROR";
+    }
+    
+    if(a > b){
+        [a,b] = [b,a];
+    }
+    let finalSum = 0;
+    for (let i = a; i <=b; i++){
+        finalSum += i;
+        
+    }
+    
+return finalSum;
+   
 
 };
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
