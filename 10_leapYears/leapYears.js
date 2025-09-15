947b99e9c1978ba4959aa958c2305a0ecf2f1d72
Jade-Evans <jpgabb@msn.com>
2025-09-15 14:26:44 +0100

    add completed leap year exercise testing conditionals

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,18 +1,14 @@
-const leapYears = function(year) {
-    let isLeapYear;
-    if (year%4 !== 0){
-        isLeapYear = false;
-    }
-    else if (year%100 ===0 && year%400===0){
-        isLeapYear = true;
-    }
-    else if (year%100===0){
-        isLeapYear = false;
-    }
-    else  isLeapYear = true;
-    return isLeapYear;
-};
+const leapYears = function(year){
+    if(year %4!==0){return false}
+    else if(year%100===0 && year%400!==0){//if divisible by 4 and 100 && not 400
+        return false;}
 
+        
+    
+    else return true;
+};
+console.log(leapYears(2004));
+   
 
 
 // Do not edit below this line
