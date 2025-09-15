a1a7b791a24d8f53c1910476d929f4d21b22053a
Jade-Evans <jpgabb@msn.com>
2025-09-15 10:27:23 +0100

    add solution to array exercise for testing .forEach, includes and push methods

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+// const removeFromArray = (array, ...args) => {
+
+//     let newArray = [];
+//     array.forEach((item) => {
+//         if (!args.includes(item)) {
+//             newArray.push(item);
+//         };
+        
+
+//     });
+//     console.log(newArray);
+// };
+//     removeFromArray([1,2,3,4], "3");
+const removeFromArray = function(array,...args){
+  const newArray = [];
+  array.forEach((item)=>{
+    if(!args.includes(item)){
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
+console.log(removeFromArray([1,2,3,4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
