bbcee72da528f2242f01193d7e49703faadbd5ce
Jade-Evans <jpgabb@msn.com>
2025-09-15 13:53:31 +0100

    add completed array exercise practicing fns, loops and conditions

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,23 +1,20 @@
-const sumAll = function(a,b) {
-    if (a <0 || b<0 || a%1 !== 0 || b%1 !==0 || typeof a !=="number" || typeof b !=="number"){
-    
-        return "ERROR";
-    }
-    
-    if(a > b){
-        [a,b] = [b,a];
-    }
-    let finalSum = 0;
-    for (let i = a; i <=b; i++){
-        finalSum += i;
-        
-    }
-    
-return finalSum;
-   
 
+const sumAll = function(a,b) {
+    if(a<0||!Number.isInteger(a)|| b<0 ||!Number.isInteger(b)){
+        return ("ERROR");
+    }
+    if(a>b){
+        [a,b]=[b,a];
+    }
+
+let total = 0;
+for(let i=a; i<=b; i++){
+    total +=i;
 };
-console.log(sumAll(1,4));
+return total;
+};
+
+console.log(sumAll(-10,4)) ; 
 
 // Do not edit below this line
 module.exports = sumAll;
