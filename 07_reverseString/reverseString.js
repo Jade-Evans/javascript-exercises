67ec21d64a1ac8aa654a146885d2aa8af5725232
Jade-Evans <jpgabb@msn.com>
2025-09-15 10:28:43 +0100

    add solution to array exercise for practicing reverse, split and join methods

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,7 +1,13 @@
-const reverseString = function(string) {
-    return string.split(" ").reverse(" ").join(" ");
 
-};
+// Pretty simple, write a function called `reverseString` that returns its input, reversed!
 
+// ```javascript
+// reverseString('hello there') // returns 'ereht olleh'
+
+const reverseString = function(string){
+    const newArray = string.split("").reverse().join("");
+    return newArray;
+}
+console.log(reverseString("hello"));
 // Do not edit below this line
 module.exports = reverseString;
