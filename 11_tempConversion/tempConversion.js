be95b6e1b771fc7a79e0092b75744e01f010a5d3
Jade-Evans <jpgabb@msn.com>
2025-09-17 10:58:36 +0100

    add solution to temp conversion exercise testing knowledge of converting data types and conditionals

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let celsius = (x - 32)*(5/9);
+  if(celsius%1===0){
+    return Math.floor(celsius)
+    }
+  else{
+    return Number(celsius.toFixed(1));}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+  let faren = (y*(9/5))+32;
+  if(faren%1===0){
+    return Math.floor(faren)
+    }
+  else{
+    return Number(faren.toFixed(1));
+  };
 };
 
 // Do not edit below this line
